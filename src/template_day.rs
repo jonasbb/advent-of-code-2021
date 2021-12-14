36566d557063a5ab3c5339f47c2413c60f25f0c9
Jonas Bushart <jonas@bushart.org>
2021-12-14 20:43:29 +0000

    Update template

--- src/template_day.rs
+++ src/template_day.rs
@@ -11,37 +11,38 @@ fn part1(input: &[u32]) -> Result<u32> {
     todo!()
 }
 
-#[aoc_runner_derive::aoc(day1, part2)]
-fn part2(input: &[u32]) -> Result<u32> {
-    todo!()
-}
+// #[aoc_runner_derive::aoc(day1, part2)]
+// fn part2(input: &[u32]) -> Result<u32> {
+//     todo!()
+// }
+
+#[cfg(test)]
+static TEST_INPUT_1: &str = r"";
 
 #[test]
 fn test_part1() -> Result<()> {
-    let input = todo!();
-    let values = input_generator(input)?;
+    let values = input_generator(TEST_INPUT_1)?;
     assert_eq!(todo!(), part1(&values)?);
     Ok(())
 }
 
-#[test]
-fn test_part1_solution() -> Result<()> {
-    let values = input_generator(include_str!("../input/2021/day1.txt").trim())?;
-    assert_eq!(todo!(), part1(&values)?);
-    Ok(())
-}
+// #[test]
+// fn test_part1_solution() -> Result<()> {
+//     let values = input_generator(include_str!("../input/2021/day1.txt").trim())?;
+//     assert_eq!(todo!(), part1(&values)?);
+//     Ok(())
+// }
 
-#[test]
-fn test_part2() -> Result<()> {
-    let input = todo!();
-    let values = input_generator(input)?;
-    assert_eq!(todo!(), part2(&values)?);
-    Ok(())
-}
+// #[test]
+// fn test_part2() -> Result<()> {
+//     let values = input_generator(TEST_INPUT_1)?;
+//     assert_eq!(todo!(), part2(&values)?);
+//     Ok(())
+// }
 
-#[test]
-fn test_part2_solution() -> Result<()> {
-    let values = input_generator(include_str!("../input/2021/day1.txt").trim())?;
-    assert_eq!(todo!(), part2(&values)?);
-    Ok(())
-}
+// #[test]
+// fn test_part2_solution() -> Result<()> {
+//     let values = input_generator(include_str!("../input/2021/day1.txt").trim())?;
+//     assert_eq!(todo!(), part2(&values)?);
+//     Ok(())
+// }
